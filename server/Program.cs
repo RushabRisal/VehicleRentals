42281b80e74ca643a91eb15cdf6048aa59ca8e72
RushabRisal <rushabofficials@gmail.com>
2026-02-02 10:04:02 +0545

    add/ENv: Setup Env

--- server/Program.cs
+++ server/Program.cs
@@ -1,19 +1,23 @@
+using DotNetEnv;
 var builder = WebApplication.CreateBuilder(args);
 
-
+//Variable declaration and definition..
 var MyAllowance = "_MyAllowance";
-
+Env.Load();
+// builder.Configuration.AddEnvironmentVariables();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "4000";
 // The way of using CORS in middleware name policy...
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name:MyAllowance,
             policy =>
             {
-                policy.WithOrigins("http://localhost:5173");
+                policy.WithOrigins("http://localhost:5173")
+                .AllowCredentials();
             });
 });
 var app = builder.Build();
 app.UseCors(MyAllowance);
 app.MapGet("/", () => "Hello World");
-
+app.Urls.Add($"http://localhost:{port}");
 app.Run();
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,10 @@ import { useState } from "react"
 function App() {
   const [value,SetValue] = useState<any>("");
   const fetchHello = () =>{
-      fetch("http://localhost:5257/")
+      fetch("http://localhost:3000/",{
+        credentials:"include",
+        method:"GET",
+      })
         .then(res => res.text())
         .then(data => SetValue(data));
       console.log(value);
